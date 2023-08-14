658662808fe56c45e02eb804c11007112dddde5d
Eric Wustrow <ewust@colorado.edu>
2023-08-14 21:54:02 +0000

    Disable compression

--- server.py
+++ server.py
@@ -3,13 +3,13 @@ import asyncio
 from websockets.server import serve
 
 async def echo(websocket):
-    print('Connection?', websocket)
+    print('Connection: ', websocket.remote_address, websocket.path)
     async for message in websocket:
         print('Got message: ', message)
         await websocket.send(message)
 
 async def main():
-    async with serve(echo, "localhost", 3000):
+    async with serve(echo, "localhost", 3000, compression=None):
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
